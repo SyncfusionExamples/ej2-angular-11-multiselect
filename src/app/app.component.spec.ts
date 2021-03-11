33b7f2dff0595c8880358ce878554f2ff858c753
Saranya Murugasamy <saranya.murugasamy@syncfusion.com>
2021-03-11 09:48:23 +0530

    Samples changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular11DropDown-app'`, () => {
+  it(`should have as title 'angular11MultiSelect-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular11DropDown-app');
+    expect(app.title).toEqual('angular11MultiSelect-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular11DropDown-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular11MultiSelect-app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular11DropDown-app';
-  // define the JSON of data
+  title = 'angular11MultiSelect-app';
   public countries: { [key: string]: Object; }[] = [
     { Name: 'Australia', Code: 'AU' },
-    { Name: 'Bermuda', Code: 'BM' },
-    { Name: 'Canada', Code: 'CA' },
-    { Name: 'Cameroon', Code: 'CM' },
-    { Name: 'Denmark', Code: 'DK' },
-    { Name: 'France', Code: 'FR' },
-    { Name: 'Finland', Code: 'FI' },
-    { Name: 'Germany', Code: 'DE' }, 
-];
-// maps the local data column to fields property
-public localFields: Object = { text: 'Name', value: 'Code' };
-// set the placeholder to Dropdown List input element
-public localWaterMark: string = 'Select countries';
+        { Name: 'Bermuda', Code: 'BM' },
+        { Name: 'Canada', Code: 'CA' },
+        { Name: 'Cameroon', Code: 'CM' },
+        { Name: 'Denmark', Code: 'DK' },
+        { Name: 'France', Code: 'FR' },
+        { Name: 'Finland', Code: 'FI' },
+        { Name: 'Germany', Code: 'DE' },          
+    ];
+    // maps the local data column to fields property
+    public localFields: Object = { text: 'Name', value: 'Code' };
+    // set the placeholder to MultiSelect Dropdown input element
+    public localWaterMark: string = 'Select countries';
 }
